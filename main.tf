f91cc5fb0147ac547d911f38b9fa02a9c1665247
tsporthd <tsporthd@gmail.com>
2021-03-07 15:46:52 -0500

    Add variable declaration

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@
 
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "aws_region" {}
 variable "ssh_private_key" {}
 variable "key_name" {}
 
@@ -12,8 +13,8 @@ variable "key_name" {}
 # PROVIDERS
 ########################################
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region = var.aws_region
 }
 
