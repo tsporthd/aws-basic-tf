75b6390c8035dce18a9e532c9be83f09ce589462
tsporthd <tsporthd@gmail.com>
2021-03-07 16:28:54 -0500

    Work with variables again

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
+#####################################
+# VARIABLE
+####################################
 
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "ssh_private_key" {}
+variable "key_name" {}
 
 
 ########################################
--- variables.tf
+++
@@ -1,9 +0,0 @@
-#####################################
-# VARIABLE
-####################################
-
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
-variable "ssh_private_key" {}
-variable "key_name" {}
