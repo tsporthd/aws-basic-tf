5e37cd0c829815b803de69ea0d6171fd6bd5278d
tsporthd <tsporthd@gmail.com>
2021-03-07 16:36:02 -0500

    Work with variables again

--- main.tf
+++ main.tf
@@ -14,9 +14,6 @@ variable "key_name" {}
 # PROVIDERS
 ########################################
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region = var.aws_region
 }
 
 
