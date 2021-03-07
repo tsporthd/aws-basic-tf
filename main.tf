ba095b730528d6da69f3635232d0bf69c7b64d62
tsporthd <tsporthd@gmail.com>
2021-03-07 15:45:35 -0500

    Add variable declaration

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 ########################################
 # VARIABLES - Defined in TFC
 ########################################
-# variable "ssh_private_key" {}
-# variable "key_name" {}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "ssh_private_key" {}
+variable "key_name" {}
 
 
 ########################################
